fa517c810a4c5c6541d7ef74f79aaf6238028b0b
Oba <adelusiobafunsho@gmail.com>
2023-06-23 22:27:24 -0500

    Coming soon dashboard

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,5 +1,39 @@
+import PageHeading from "../components/PageHeading";
+
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <div className="DashboardPage">
+      <PageHeading title="Dashboard" />
+      <div className="Page__container">
+        <div className="Page__section-container">
+          <div className="Page__section">
+            <h2>Dashboard coming soon</h2>
+            <p>Check out other pages...</p>
+            <ul style={{ listStyle: "inside" }}>
+              <li>
+                <a href="components/accordion">Accordion</a>
+              </li>
+              <li>
+                <a href="components/alerts">Alerts</a>
+              </li>
+              <li>
+                <a href="components/badges">Badges</a>
+              </li>
+              <li>
+                <a href="components/buttons">Buttons</a>
+              </li>
+              <li>
+                <a href="components/cards">Cards</a>
+              </li>
+              <li>
+                <a href="components/chips">Chips</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
