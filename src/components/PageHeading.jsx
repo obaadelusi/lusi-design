31c5374f1f37a0f4620e2a3eb41d12e18390c0af
Oba <adelusiobafunsho@gmail.com>
2023-06-24 20:28:36 -0500

    Implemented breadcrumbs

--- src/components/PageHeading.jsx
+++ src/components/PageHeading.jsx
@@ -1,11 +1,24 @@
+import { useLocation, Link } from "react-router-dom";
 import "./PageHeading.scss";
 
 const PageHeading = ({ title }) => {
+  const { pathname } = useLocation();
+
+  function getBreadcrumbs() {
+    let pathArray = pathname.split("/");
+    for (let i = 0; i < pathArray.length; i++) {
+      pathArray[i] = pathArray[i].charAt(0).toUpperCase() + pathArray[i].slice(1);
+    }
+
+    return pathArray.join(" / ");
+  }
+
   return (
     <div className="PageHeading">
       <h1 className="PageHeading-title">{title}</h1>
       <div className="PageHeading-breadcrumbs">
-        <a href="/dashboard">Dashboard</a> / Components / Breadcrumbs
+        <Link to="/dashboard">Home</Link>
+        {getBreadcrumbs()}
       </div>
     </div>
   );
